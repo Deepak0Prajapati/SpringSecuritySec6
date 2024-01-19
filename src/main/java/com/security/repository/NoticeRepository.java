d95f2a061999ae1e798fe15b78539fc93e59fb68
unknown <Administrator@DESKTOP-TBGGLD3.bluethink.inc>
2024-01-19 13:25:32 +0530

    new changes

--- src/main/java/com/security/repository/NoticeRepository.java
+++ src/main/java/com/security/repository/NoticeRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.security.model.Notice;
 
-public interface NoticeRepository extends CrudRepository<Notice, Long>{
+public interface NoticeRepository extends CrudRepository<Notice, Integer>{
 	
 	@Query(value = "from Notice n where CURDATE() BETWEEN noticBegDt AND noticEndDt")
 	List<Notice> findAllActiveNotices();
