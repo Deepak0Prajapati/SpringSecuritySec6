f5aa4d40d9cab681eb4c5fbda12b9c13fc1aa3ff
unknown <Administrator@DESKTOP-TBGGLD3.bluethink.inc>
2024-01-22 17:27:57 +0530

    jwt token implementation

--- src/main/java/com/security/config/ProjectSecurityConfig.java
+++ src/main/java/com/security/config/ProjectSecurityConfig.java
@@ -31,8 +31,9 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
         requestHandler.setCsrfRequestAttributeName("_csrf");
-        http.securityContext((context) -> context.requireExplicitSave(false))
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+        
+        
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
