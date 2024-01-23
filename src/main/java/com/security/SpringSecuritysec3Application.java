6556a71f84fb1d2ad78800827c3bc207c24a238e
Deepak0Prajapati <deepakprajapatibluethink@gmail.com>
2024-01-23 19:42:27 +0530

    method level security impl

--- src/main/java/com/security/SpringSecuritysec3Application.java
+++ src/main/java/com/security/SpringSecuritysec3Application.java
@@ -2,10 +2,11 @@ package com.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity(debug = true)
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SpringSecuritysec3Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/security/repository/LoanRepository.java
+++ src/main/java/com/security/repository/LoanRepository.java
@@ -3,11 +3,13 @@ package com.security.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.security.model.Loans;
 
 public interface LoanRepository  extends CrudRepository<Loans, Integer>{
 	
+//	@PreAuthorize("hasRole('USER')")	
 	List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
 
 }
