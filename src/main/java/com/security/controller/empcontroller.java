bd44d83ff498af246b1f61f4a4cf912991ccbaf4
Deepak0Prajapati <deepakprajapatibluethink@gmail.com>
2024-01-23 13:24:13 +0530

    desktop change

--- src/main/java/com/security/controller/empcontroller.java
+++ src/main/java/com/security/controller/empcontroller.java
@@ -1,7 +1,6 @@
 package com.security.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
